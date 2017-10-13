eb613ed6cb3b9acdf5a244cedd0304262bf7f979
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-10-13 15:20:14 +0200

    Routing done

--- Web_Application/Routing/src/app/home.component.ts
+++ Web_Application/Routing/src/app/home.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  private statusmessage: string;
+  private warning_message: string;
+  private error_message: string;
+
+  public constructor() {
+    this.statusmessage = `Dummytext: Sunt enim minim cupidatat deserunt ullamco dolore eiusmod
+      fugiat eu quis aliquip mollit labore. Aute fugiat fugiat veniam cupidatat minim est irure
+      est ipsum. Duis exercitation qui quis proident ut exercitation. Ad labore sint ullamco est
+      in proident labore consectetur ea et in nisi. Ullamco magna tempor ipsum occaecat do eu
+      adipisicing. Cillum consequat mollit dolore proident commodo.`;
+    this.error_message = `This is an errormessage with a very long text, so I can see, that it
+      automatically breaks the line, if it is too long.`
+    this.warning_message = 'Some warnings may occure.'
+  }
 }
