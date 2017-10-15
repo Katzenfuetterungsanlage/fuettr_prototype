5b84700b044ac28b6311748a22ccdcc8adf2e2dd
BuildTools <unconfigured@null.spigotmc.org>
2017-10-15 16:09:05 +0200

    server.listen(port, () => console.log(`\n ----> Server running on localhost:${port}`));

--- Web_Application/mean-app/server.js
+++ Web_Application/mean-app/server.js
@@ -38,4 +38,4 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+server.listen(port, () => console.log(`\n ----> Server running on localhost:${port}`));
