8a39dd5419fafb6ba3de989a1a8ed094a90d78d8
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-10-23 20:25:06 +0200

    update updated

--- Web_Application/mean-app2/ng2/src/app/update.service.ts
+++ Web_Application/mean-app2/ng2/src/app/update.service.ts
@@ -6,7 +6,30 @@ export class UpdateService {
 
   constructor(private http: Http) { }
 
-  getUpdate() {
-    this.http.get('http://localhost/api/getUpdate');
+  public getUpdate(): number {
+    const err = new MyError('test', 100);
+    console.log(err);
+    this.http.get('http://localhost/api/getUpdate').toPromise()
+      .then(response => {
+        console.log('Update...');
+      })
+      .catch(error => {
+        if (error instanceof Error) {
+          console.log(error);
+        } else {
+          console.log('?');
+        }
+      });
+    return 0;
+  }
+}
+
+class MyError extends Error {
+  constructor(message: string, private x: number) {
+    super(message);
+  }
+
+  public toString(): string {
+    return this.message + ', x=' + this.x;
   }
 }
