a3dd30c7b751eb7fd9597fd15c5e68e9916a5b5b
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-10-24 10:09:48 +0200

    version

--- Web_Application/mean-app2/ng2/src/app/update.service.ts
+++ Web_Application/mean-app2/ng2/src/app/update.service.ts
@@ -3,15 +3,23 @@ import { Http } from '@angular/http';
 
 @Injectable()
 export class UpdateService {
-  private Url = 'api/getUpdate';  // URL to web api
+  private getUrl = 'api/getUpdate';  // URL to web api
+  private checkUrl = '';  // URL to web api
 
   constructor(private http: Http) { }
 
   getUpdate(): Promise<any> {
-    return this.http.get(this.Url)
+    return this.http.get(this.getUrl)
       .toPromise()
       .then(response => response.json())
       .catch(this.handleError);
+  }
+
+  checkUpdate(): Promise<any> {
+    return this.http.get(this.checkUrl)
+    .toPromise()
+    .then(response => response.json())
+    .catch(this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
