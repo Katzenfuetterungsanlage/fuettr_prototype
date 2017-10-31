ddf6b0e6e2a7c06197bcb0e9f7847d27c05bc3ea
harflm13 <harflm13@htl-kaindorf.ac.at>
2017-10-31 16:58:29 +0100

    Zeiten speichern funzt

---
+++ Java_Application/Java/src/methods/StreamReader.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package methods;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Florian
+ */
+public class StreamReader
+{
+    public String einlesen(String pfad)
+    {
+        String text = null;
+        
+        try (final BufferedReader reader = new BufferedReader( //fasst in blöcke zusammen
+            new InputStreamReader(new FileInputStream(pfad),"utf8"))) 
+            //in doInBackground kann nur auf final Objekte zugegriffen werden - file2
+        {
+            text = reader.readLine(); 
+            
+            System.out.println("Streams wurden erfolgreich gelesen!");
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        
+        return text;
+    }
+}
+
