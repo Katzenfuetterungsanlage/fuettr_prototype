ec40ba5b3fc2c1685a6db2c29d0ccec357b7775a
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-10-31 17:35:20 +0100

    script changed

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # install script for Fuettr Server and Java-Application
 
+echo "setting git credentials..."
 sudo git config --global user.name "fuettr"
 sudo git config --global user.email "fuettr@gmail.com"
-cd /home/pi/git/fuettr_prototype && sudo rsync -aP /home/pi/git/fuettr_prototype/rc.local /etc/rc.local && sudo reboot
+
+echo "copying rc.local"
+cd /home/pi/git/fuettr_prototype && sudo rsync -aP /home/pi/git/fuettr_prototype/rc.local /etc/rc.local
+
+echo "done"
+echo "please reboot your system to start the applications"
