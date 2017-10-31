039dee5556b03a6e9dd18b6820c0f709292d40c1
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-10-31 17:36:26 +0100

    install script changed

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ echo "copying rc.local"
 cd /home/pi/git/fuettr_prototype && sudo rsync -aP /home/pi/git/fuettr_prototype/rc.local /etc/rc.local
 
 echo "done"
-echo "please reboot your system to start the applications"
+echo "please reboot your system to start the applications (sudo reboot)"
