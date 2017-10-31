830cb37f3066616df67d05395efd799dca8925ff
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-10-31 17:43:25 +0100

    install script updated

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo git config --global user.name "fuettr"
 sudo git config --global user.email "fuettr@gmail.com"
 
 echo "copying rc.local"
-cd /home/pi/git/fuettr_prototype && sudo rsync -aP /home/pi/git/fuettr_prototype/rc.local /etc/rc.local
+sudo rsync -aP /home/pi/git/fuettr_prototype/rc.local /etc/rc.local
 
 echo "done"
 echo "please reboot your system to start the applications (sudo reboot)"
