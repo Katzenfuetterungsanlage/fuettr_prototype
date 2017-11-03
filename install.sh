1ddb7a5688334fd0a6e01007a049d7c8d0769ca1
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-11-03 02:29:22 +0200

    install reworked

--- install.sh
+++ install.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 # install script for Fuettr Server and Java-Application
 
+home = ${HOME}
+
+echo "Home:" $home
+
 echo "setting git credentials..."
 sudo git config --global user.name "fuettr"
 sudo git config --global user.email "fuettr@gmail.com"
 
-echo "making sure the latest version is downloaded..."
-sudo git pull
+echo "downloading..."
+cd $home && mkdir git && cd git && git clone https://github.com/Katzenfuetterungsanlage/fuettr_prototype.git
 
 echo "copying rc.local..."
-sudo rsync -aP /home/pi/git/fuettr_prototype/rc.local /etc/rc.local
+sudo rsync -aP $home/git/fuettr_prototype/rc.local /etc/rc.local
+
+echo "installing node dependencies..."
+sudo npm install -g @angular/cli gulp
+cd $home/git/Web_Application/Webserver/ng2 && sudo npm install
+cd $home/git/Web_Application/Webserver/server && sudo npm install
+
+echo "building server..."
+cd $home/git/Web_Application/Webserver/ng2 && ng build
+cd $home/git/Web_Application/Webserver/server && gulp cleanAndBuild
+
+echo "starting server..."
+cd $home/git/Web_Application/Webserver/server/dist && sudo node main.js
 
 echo "done"
-echo "please reboot your system to start the applications (sudo reboot)"
