50f1fce79db5a46c25a0d9f2ff010b7df1c610d2
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-11-03 17:16:42 +0100

    whoami

--- Web_Application/Webserver/server/src/main.ts
+++ Web_Application/Webserver/server/src/main.ts
@@ -82,7 +82,7 @@ function errorHandler(err: express.Errback, req: express.Request, res: express.R
 }
 
 function update() {
-  child.exec(`cd .. && ls`, (error, stdout, stderr) => {
+  child.exec(`cd .. && ls && whoami`, (error, stdout, stderr) => {
       if (stdout !== '') {
         debug.info(stdout);
       }
