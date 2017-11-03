b9e9dd2ef4dbca7ec09c57b366521ec60bd089a6
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-11-03 02:51:39 +0200

    colors

--- install.sh
+++ install.sh
@@ -3,26 +3,26 @@
 
 THISUSER=/home/$USER
 
-echo -e "setting git credentials... \e[44mBlue"
+echo -e "\e[44msetting git credentials...\e[0m"
 sudo git config --global user.name "fuettr"
 sudo git config --global user.email "fuettr@gmail.com"
 
-echo -e "downloading... \e[44mBlue"
+echo -e "\e[44mdownloading...\e[0m"
 cd $THISUSER && mkdir git && cd git && git clone https://github.com/Katzenfuetterungsanlage/fuettr_prototype.git
 
-echo -e "copying rc.local... \e[44mBlue"
+echo -e "\e[44mcopying rc.local...\e[0m"
 sudo rsync -aP $THISUSER/git/fuettr_prototype/rc.local /etc/rc.local
 
-echo -e "installing node dependencies... \e[44mBlue"
+echo -e "\e[44minstalling node dependencies...\e[0m"
 sudo npm install -g @angular/cli gulp
 cd $THISUSER/git/Web_Application/Webserver/ng2 && sudo npm install
 cd $THISUSER/git/Web_Application/Webserver/server && sudo npm install
 
-echo -e "building server... \e[44mBlue"
+echo -e "\e[44mbuilding server...\e[0m"
 cd $THISUSER/git/Web_Application/Webserver/ng2 && ng build
 cd $THISUSER/git/Web_Application/Webserver/server && gulp cleanAndBuild
 
-echo -e "starting server... \e[44mBlue"
+echo -e "\e[44mstarting server...\e[0m"
 cd $THISUSER/git/Web_Application/Webserver/server/dist && sudo node main.js
 
-echo -e "done \e[42mGreen""
+echo -e "\e[42mdone\e[0m"
