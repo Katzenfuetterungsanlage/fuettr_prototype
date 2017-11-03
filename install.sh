21e0f3e9b41924b5dec20159ecd23606d2122158
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-11-03 02:44:44 +0200

    wörks?

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 # install script for Fuettr Server and Java-Application
 
-USER = /home/$HOME
+THISUSER = /home/$USER
 
-echo "Home directory: $USER"
+echo "Home directory: $THISUSER"
 
 echo "setting git credentials..."
 sudo git config --global user.name "fuettr"
 sudo git config --global user.email "fuettr@gmail.com"
 
 echo "downloading..."
-cd $USER && mkdir git && cd git && git clone https://github.com/Katzenfuetterungsanlage/fuettr_prototype.git
+cd $THISUSER && mkdir git && cd git && git clone https://github.com/Katzenfuetterungsanlage/fuettr_prototype.git
 
 echo "copying rc.local..."
-sudo rsync -aP $USER/git/fuettr_prototype/rc.local /etc/rc.local
+sudo rsync -aP $THISUSER/git/fuettr_prototype/rc.local /etc/rc.local
 
 echo "installing node dependencies..."
 sudo npm install -g @angular/cli gulp
-cd $USER/git/Web_Application/Webserver/ng2 && sudo npm install
-cd $USER/git/Web_Application/Webserver/server && sudo npm install
+cd $THISUSER/git/Web_Application/Webserver/ng2 && sudo npm install
+cd $THISUSER/git/Web_Application/Webserver/server && sudo npm install
 
 echo "building server..."
-cd $USER/git/Web_Application/Webserver/ng2 && ng build
-cd $USER/git/Web_Application/Webserver/server && gulp cleanAndBuild
+cd $THISUSER/git/Web_Application/Webserver/ng2 && ng build
+cd $THISUSER/git/Web_Application/Webserver/server && gulp cleanAndBuild
 
 echo "starting server..."
-cd $USER/git/Web_Application/Webserver/server/dist && sudo node main.js
+cd $THISUSER/git/Web_Application/Webserver/server/dist && sudo node main.js
 
 echo "done"
