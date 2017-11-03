f970d43d3c8012d6286eb58a527412d233f99a87
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-11-03 17:10:27 +0100

    update script

--- Web_Application/Webserver/server/src/main.ts
+++ Web_Application/Webserver/server/src/main.ts
@@ -82,10 +82,7 @@ function errorHandler(err: express.Errback, req: express.Request, res: express.R
 }
 
 function update() {
-  child.exec(`cd /home/pi/git/fuettr_prototype && git reset --hard && git pull &&
-  cd ng2 && npm i && cd ../server && npm i &&
-  sudo rsync -aP /home/pi/git/fuettr_prototype/rc.local /etc/rc.local &&
-  sudo reboot`, (error, stdout, stderr) => {
+  child.exec(`cd .. && bash update`, (error, stdout, stderr) => {
       if (stdout !== '') {
         debug.info(stdout);
       }
